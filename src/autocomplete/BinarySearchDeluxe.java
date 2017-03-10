44190d28f61d975826ac45958cf0d79be28c447c
Ethan Young <young.e.f42@gmail.com>
2017-03-10 01:09:00 -0800

    slightly changed BSD stubs

--- src/autocomplete/BinarySearchDeluxe.java
+++ src/autocomplete/BinarySearchDeluxe.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class BinarySearchDeluxe {
 	// Returns the index of the first key in a[] that equals the search key, or -1 if no
 	//such key.
-	 public static <Key extends Comparable<Key>> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+	 public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
 		 return 0;
 	 }
 	 // Returns the index of the last key in a[] that equals the search key, or -1 if no
 	//such key.
-	 public static <Key extends Comparable<Key>> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+	 public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
 		 return 0;
 	 }
 	 // unit testing (required)
